5e2a3fc8e6aaae4ba043a194355dc2c6e7fc54e9
Simone D <monatru3@hotmail.com>
2018-01-30 23:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var recipes= {
 
-
+  function updateObjectWithKeyAndValue(object, key, value) {
+    Object.assign({}, object, { [key]: value })
+    return object
+  }
 
 
 };
-function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, object, { [key]: value })
-  return object
-}
