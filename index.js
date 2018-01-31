1ab3a3114cd6cef45db34099267729b13b8c2cda
Simone D <monatru3@hotmail.com>
2018-01-31 00:12:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   Object.assign(object, { [key]: value})
   return object
 }
